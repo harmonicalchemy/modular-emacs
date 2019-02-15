33ba67766e55c70c5a832fbf0bf413598d698174
Alisha Awen Sheppard <Siren1@disroot.org>
2019-02-14 20:26:34 -0800

    added more support for ssh-config-mode

--- lisp/modules/04-devOps-pkg-conf.el
+++ lisp/modules/04-devOps-pkg-conf.el
@@ -28,8 +28,15 @@
 ;; Start undo-tree:
 (global-undo-tree-mode)
 
+;; Load ssh-config-mode:
+(autoload 'ssh-config-mode "ssh-config-mode" t)
+
 ;; Automatically sense SSH config files and set ssh-config-mode:
- (add-to-list 'auto-mode-alist '("~/.ssh/config\\'" . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/\\.ssh/config\\'"     . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/sshd?_config\\'"      . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/known_hosts\\'"       . ssh-known-hosts-mode))
+(add-to-list 'auto-mode-alist '("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
+(add-hook 'ssh-config-mode-hook 'turn-on-font-lock)
 
 ;; Enforce spaces for indentation, instead of tabs
 (setq-default indent-tabs-mode nil)
