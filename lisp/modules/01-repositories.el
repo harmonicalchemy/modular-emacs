f0d776a798ad502b6d3dd8d2df87125839e5eaf9
Alisha Awen Sheppard <Siren1@disroot.org>
2019-06-22 20:17:05 -0700

    Changed 01-repositories.el.  Added logic and also pinned library to stay compataable with CIDER. (if it gets installed optionally)

--- lisp/modules/01-repositories.el
+++ lisp/modules/01-repositories.el
@@ -11,20 +11,41 @@
 ;; Load package.el - Modified 2019-002-05 (Added the 3 Main Emacs package Repositories)
 (require 'package)
 
-;;Add GNU elpa to list of repositories:
-(add-to-list 'package-archives
-	     '("elpa" . "https://eelpa.gnu.org/packages/") 
-             t)
+
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://stable.melpa.org/packages/")
+        ("melpa-unstable" . "https://melpa.org/packages/")))
+
+(setq package-archive-priorities
+      '(("melpa" . 50)
+        ("gnu" . 10)
+        ("melpa-unstable" . 0)))
+
+;; Pin required for CIDER compatability: (if it gets installed later)
+
+(add-to-list 'package-pinned-packages '(clj-refactor . "melpa-unstable") t)
 
 ;;Add melpa-stable to list of repositories:
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/")
-             t)
+;(add-to-list 'package-archives
+;            '("melpa-stable" . "https://stable.melpa.org/packages/")
+;           t)
 
 ;;Add melpa to list of repositories:
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") 
-             t)
+;(add-to-list 'package-archives
+;	     '("melpa" . "https://melpa.org/packages/") 
+;            t)
 
-;;Initialize package.el
+;;Add GNU elpa to list of repositories:
+;(add-to-list 'package-archives
+;	     '("elpa" . "https://eelpa.gnu.org/packages/") 
+;            t)
+
+
+
+
+
+
+;;Initialize package.el:
+
 (package-initialize)
