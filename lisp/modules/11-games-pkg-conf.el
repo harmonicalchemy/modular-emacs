421f67a6f551248dd24a4847644cbe19522fb005
Alisha Awen <Siren1@disroot.org>
2020-07-04 23:07:33 -0700

    [Rodrigo]: Added new functionality to emms section of 11=games-pkg-conf.el

--- lisp/modules/11-games-pkg-conf.el
+++ lisp/modules/11-games-pkg-conf.el
@@ -26,7 +26,8 @@
 
 ;; Declare a list of required packages for extra dired features:
 (defvar me--req-games-packages
-  '(xterm-color))
+  '(emms
+    xterm-color))
 
 ;; Install required packages:
 (mapc (lambda (p)
@@ -48,15 +49,54 @@
 (olivetti-set-width 102)
 (goto-address-mode t)))          ;; ... clickable links
 
+
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; Setup & Configure GNU EMMS (Emacs Multimedia System)
 ;; Reference: https://www.gnu.org/software/emms
+;; EMMS Manual: https://www.gnu.org/software/emms/manual/
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-(add-to-list 'load-path "~/.emacs.d/lisp/my-modules/emms")
-(require 'emms-setup)
-(emms-all)
-(emms-default-players)
+(autoload 'emms-smart-browse "emms-browser.el" "Browse with EMMS" t)
+(global-set-key [(f7)] 'emms-smart-browse)
+
+(with-eval-after-load 'emms
+  (emms-standard) ;; or (emms-devel) if you want all features
+  ;; NOTE: Change this next Path definition to the location of your own Media Library:
+  (setq emms-source-file-default-directory "~/Documents/DATA/000-HAP-Media-Library/010-MP3-Library"
+        emms-info-asynchronously t
+        emms-show-format "♪ %s")
+
+  ;; Might want to check `emms-info-functions',
+  ;; `emms-info-libtag-program-name',
+  ;; `emms-source-file-directory-tree-function'
+  ;; as well.
+
+  (require 'emms-setup)
+  (emms-all)
+  (emms-mode-line nil)
+  (require 'helm)
+
+  ;; Determine which player to use.
+  ;; If you don't have strong preferences or don't have
+  ;; exotic files from the past (wma) `emms-default-players`
+  ;; is probably all you need.
+  (if (executable-find "mpd")
+      (progn
+        (require 'emms-player-mpd)
+        (add-to-list 'emms-player-list 'emms-player-mpd)
+        (require 'emms-volume)
+        (setq emms-volume-change-function 'emms-volume-mpd-change))
+    (emms-default-players))
+
+  ;; For libre.fm see `emms-librefm-scrobbler-username' and
+  ;; `emms-librefm-scrobbler-password'.
+  ;; Future versions will use .authoinfo.gpg.
+  )
+
+(global-set-key (kbd "C-c C-p") 'emms-pause)
+(global-set-key (kbd "C-c C-s") 'emms-stop)
+(global-set-key (kbd "C-c <up>") 'emms-previous)
+(global-set-key (kbd "C-c <down>") 'emms-next)
 
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; END: [modular-emacs]:~/.emacs.d/lisp/modules/11-games-pkg-conf.el
